c8e16d9943a3e6088c349264a0422b785af0ec9d
Saad Zaheer <salaam7860@gmail.com>
2023-12-27 16:38:52 +0500

    add sockets

---
+++ sockets/server.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+
+
+
+def start():
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1} ")
+
+print("[SERVER] is Starting...")
+
+start()
+
